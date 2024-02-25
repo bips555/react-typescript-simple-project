e7ac129067139751d778f5f980aace4040870018
biplove subedi <subedi.biplove10@gmail.com>
2024-02-25 14:23:19 +0545

    interface created for use state hook

--- todotypescriptreact/src/App.tsx
+++ todotypescriptreact/src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './Components/InputField';
 
 
 const  App:React.FC=()=>{
+  const [todo , setTodo] = useState<string>("");
   return (
    <>
  <div className="App">
   <span className='heading'>Taskify</span>
-  <InputField/>  
+  <InputField todo={todo} setTodo={setTodo}/>  
   </div> 
    </>
   );
--- todotypescriptreact/src/Components/InputField.tsx
+++ todotypescriptreact/src/Components/InputField.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-
-export default function InputField() {
+import React from "react";
+import "./styles.css";
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+export default function InputField({todo,setTodo}:Props) {
   return (
-  <form>
-    <input type='text' placeholder='enter a task' className='input_box'/>
-    <button type='submit' className='input_submit'>
+    <form>
+      <input type="text" placeholder="enter a task" className="input_box" />
+      <button type="submit" className="input_submit">
         GO
-    </button>
-  </form>
-  )
+      </button>
+    </form>
+  );
 }
